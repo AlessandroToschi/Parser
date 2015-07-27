296d4bdf1cfcfb031fc40839a3202a2d87a238e0
AlessandroToschi <AlessandroToschi1@MBPdiAlessandro.homenet.telecomitalia.it>
2015-07-27 12:09:22 +0200

    Initial Commit

---
+++ Parser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Parser
+//
+//  Created by Alessandro on 27/07/15.
+//  Copyright (c) 2015 Alessandro. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
